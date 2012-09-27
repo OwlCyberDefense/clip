5bb7fc5d1927cb7f68505e32367fcb7843f986be
Spencer Shimko <sshimko@tresys.com>
2012-09-27 16:53:34 -0400

    Bump aqueduct to r322 in upstreams/ and drop older rev

--- upstreams/aqueduct/aqueduct-trunk-r313/compliance/Puppet/STIG/rhel5/Unix-Checklist/base/dev/rhel5/puppet/trunk/etc/puppet/modules/stig/GEN001460/manifests/init.pp
+++
@@ -1,5 +0,0 @@
-class gen001460 {
-	## (GEN001460: CAT IV) (Previously - G052) The SA will ensure all home
-	## directories defined in the /etc/passwd file exist.
-	exec { "for HOMEDIR in `cut -d: -f6 /etc/passwd`;do echo mkdir -p \$HOMEDIR;done": }
-}
