a18944209f8b4f96588627e7af864ecadca52711
Spencer Shimko <sshimko@tresys.com>
2012-11-14 12:11:52 -0500

    Add support script for cleaning up pungi cruft

---
+++ support/clean-pungi-cruft.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "WARNING: This script blindly removes all Pungi-related directories from /tmp."
+echo "         If anyone is building an ISO at this time please hit ctrl-c."
+echo "         You probably need to sudo to run this script successfully."
+read
+
+pushd . 1>/dev/null
+cd /tmp;
+rm -rf keepfile* modinfo* instimage* keymaps* makeboot* yumcache*
+popd 1>/dev/null
