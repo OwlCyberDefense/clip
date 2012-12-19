e8f60c72851381076bb7398c5739364b60a30bf5
Yuli Khodorkovskiy <ykhodorkovskiy@tresys.com>
2012-12-19 11:33:22 -0500

    SELinux config remediation will now modify commented out SELINUX=permissive|disabled lines

--- packages/aqueduct/aqueduct/compliance/Bash/SSG/dev/set_selinux_state.sh
+++ packages/aqueduct/aqueduct/compliance/Bash/SSG/dev/set_selinux_state.sh
@@ -23,4 +23,4 @@ FILE="/etc/selinux/config"
 [ -f $FILE ] || exit 1
 
 . $(dirname $0)/set_general_entry
-safe_add_field "(^\s*SELINUX\s*=).*" "enforcing" $FILE
+safe_add_field "(SELINUX\s*=)permissive|disabled" "enforcing" $FILE
