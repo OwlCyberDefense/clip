c3ffa2590540f31bb64c344f18cf1be078ed4c5f
Spencer Shimko <spencer@quarksecurity.com>
2013-04-22 15:52:25 -0400

    Ask users for repo paths in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -u
 set -e
-echo -e "Creating an environment for building software and ISOs can be a little 
+/bin/echo -e "Creating an environment for building software and ISOs can be a little 
 complicated.  This script will automate some of those tasks.  Keep in mind that 
 this script isn't exhaustive; depending on a variety of factors you may have to
 install some additional packages.\n\nYour user *must* have sudo access for any 
@@ -9,6 +9,32 @@ of this to work.
 If you are using RHEL enter 'r'.
 If you are using CentOS 'c'."
 read distro
+/bin/echo -e "CLIP uses yum repositories for building packages and generting ISOs.
+These must be directories of packages, not RHN channels.  E.g. a directory with
+a bunch of packages and a repodata/ sub-directory.  If you do not have yum 
+repositories like this available CLIP will not work!  Please see Help-FAQ.txt!\n\n"
+
+tmpfile=''
+while :; do
+	/bin/echo -e "
+Enter a name for this yum repo.  Just leave empty if you are done adding, or don't wish to change, the repositories.\n"
+	read name
+	[ x"$name" == "x" ] && break
+	/bin/echo -e "
+Enter a fully qualified path for this yum repo.  Just leave empty if you are done adding, or don't wish to change, the repositories.\n"
+	read path
+	[ x"$path" == "x" ] && break
+
+	if [ x"$tmpfile" == "x" ]; then
+		tmpfile=`/bin/mktemp`
+		/bin/cat CONFIG_REPOS | /bin/sed -e 's/^\([a-zA-Z0-9].*\)$/#\1/' > $tmpfile
+	fi
+	/bin/echo -e "# INSERTED BY BOOTSTRAP.SH\n$name = $path" >> $tmpfile
+done
+
+if [ x"$tmpfile" != "x" ]; then
+	mv $tmpfile CONFIG_REPOS
+fi
 
 # install packages that we need but aren't commonly present on default RHEL installs.
 for i in createrepo rpm-build; do
@@ -17,7 +43,7 @@ done;
 
 # if we're on RHEL add the Opt channel
 if [ "$distro" == "r" ]; then
-	echo "We're going to try to subscribe to the RHEL Optional channel for your distro.
+	/bin/echo "We're going to try to subscribe to the RHEL Optional channel for your distro.
 This might not work if you don't have credentials or you're out of entitlements.
 We *NEED* packages from Opt to be installed on the build host *and* need to pull
 packages from there to put on the generated installable media.  If you're using
@@ -34,7 +60,7 @@ Press enter to continue.
 	sudo rhn-channel --add --channel=rhel-x86_64-server-optional-`/bin/awk '{ print $7; }' /etc/redhat-release`.z
 fi
 /bin/rpm -q "python-kid" >/dev/null || sudo /usr/bin/yum install -y python-kid || if [ "$?" != "0" ]; then
-	echo "WARNING: we couldn't find a package we need to install on the build 
+	/bin/echo "WARNING: we couldn't find a package we need to install on the build 
 host.  This is usually the result of using RHEL without a subscription to RHN. Try this:
 1. Grab a CentOS ISO.
 2. Mount it.
@@ -64,6 +90,8 @@ if ! rpm -q pungi >/dev/null; then
 	popd > /dev/null
 fi
 
-echo -e "Basic bootstrapping of build host is complete.\nPress 'enter' to run 'make clip-rhel6-iso' or ctrl-c to quit."
+
+
+/bin/echo -e "Basic bootstrapping of build host is complete.\nPress 'enter' to run 'make clip-rhel6-iso' or ctrl-c to quit."
 read
-make clip-rhel6-iso
+/usr/bin/make clip-rhel6-iso
