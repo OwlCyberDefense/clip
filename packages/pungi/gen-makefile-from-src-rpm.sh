1d1e77ebd553a4d814f0ec7e525652ca8b558008
Mike Palmiotto <mpalmiotto@tresys.com>
2014-08-08 16:48:31 -0400

    Bump pungi version and patch Lorax
    Anaconda has dropped anaconda-runtime scripts and moved to Lorax in rhel7.
    Pungi 2.0.22 still uses anaconda-runtime's buildinstall script, so we need
    to bump the package to one that supports Lorax (2.13-2.el7).
    
    - Use the default pungi binary (/usr/bin/pungi) and drop the patches in support/.
    - Fix a bug in Lorax's treebuilder and (safely) apply the patch before
    running pungi. This is a temporary workaround until we can add Lorax as
    a srcrpm and patch it there.

---
+++ packages/pungi/gen-makefile-from-src-rpm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -u
+set -e
+
+if [ $# -ne 1 ]; then
+	echo "Usage: `basename $0` <src rpm filename>"
+	exit 1
+fi
+
+echo "Extracting information from src rpm..."
+PKGNAME=`rpmquery -qp --queryformat '%{NAME}' $1 2>/dev/null`
+
+# We've seen cases from, e.g., rpmforge, where the src.rpm ver/release
+# don't match the contained package ver/release.  So extract the spec
+# file and get it from there.
+test -d tmp || mkdir tmp
+cd tmp
+rm -f ${PKGNAME}.spec
+( rpm2cpio ../$1 | cpio --quiet -i ${PKGNAME}.spec ) 2>&1 >/dev/null
+
+# Spec files can roll multiple packages which results in the query 
+# returning dupe versions, just go with the first.  *Hopefully* this
+# is the 99& case.
+VERSION=`rpmquery -q --specfile --queryformat '%{VERSION}\n' ${PKGNAME}.spec | head -n 1`
+RELEASE=`rpmquery -q --specfile --queryformat '%{RELEASE}\n' ${PKGNAME}.spec | head -n 1`
+ARCH=`rpmquery -q --specfile --queryformat '%{ARCH}\n' ${PKGNAME}.spec | head -n 1`
+cd ../
+
+rm -rf tmp
+
+echo "Name: $PKGNAME"
+echo "Version: $VERSION"
+echo "Release: $RELEASE"
+
+sed -e "s;SRC_SRPM :=.*;SRC_SRPM := \$(CURDIR)/$1;" -e "s;^PKGNAME :=.*;PKGNAME := $PKGNAME;" -e "s;^VERSION :=.*;VERSION := $VERSION;" -e "s;^RELEASE :=.*;RELEASE := $RELEASE;" Makefile.tmpl > Makefile 
+
+if [ x"$ARCH" == "xnoarch" ]; then
+	echo "Arch: $ARCH"
+	sed -i -e  "s;ARCH .*;ARCH := $ARCH;" Makefile 
+fi
